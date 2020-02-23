ef5e060b4fc722428bd8fac447eb945c49597a79
TimeToRave <nickkuzmin1301@gmail.com>
2020-02-23 19:37:55 +0300

    Запись строки с датой при открытии приложения

--- CyclicTracker/Tasker.cs
+++ CyclicTracker/Tasker.cs
@@ -11,28 +11,24 @@ namespace CyclicTracker
         public string CurrentTask { get => currentTask; set => currentTask = value; }
         public DateTime CurrentTaskStart { get => currentTaskStart; set => currentTaskStart = value; }
 
-        public Tasker()
-        {
-            CurrentTask = string.Empty;
-        }
+        public Tasker() : this(string.Empty) {}
 
         public Tasker(string task)
         {
             CurrentTask = task;
             CurrentTaskStart = DateTime.Now;
+            WriteToFile("[" + DateTime.Now.ToString("dd MMMM yyyy, dddd") + "]");
         }
 
         public void Save()
         {
-            using (StreamWriter writetext = new StreamWriter(@"D:\tasks.txt", true, System.Text.Encoding.UTF8))
-            {
-                writetext.WriteLine(string.Format(
+           WriteToFile(string.Format(
                     "[{0} - {1}] {2}", 
                     CurrentTaskStart.ToString("HH:mm"),
                     DateTime.Now.ToString("HH:mm"),
-                    CurrentTask
-                ));
-            }
+                    CurrentTask)
+            );
+            
             Clear();
         }
 
@@ -40,6 +36,14 @@ namespace CyclicTracker
         {
             CurrentTask = string.Empty;
             CurrentTaskStart = DateTime.MinValue;
+        }
+
+        private void WriteToFile(string stringToWrite)
+        {
+            using (StreamWriter writetext = new StreamWriter(@"D:\tasks.txt", true, System.Text.Encoding.UTF8))
+            {
+                writetext.WriteLine(stringToWrite);
+            }
         }
     }
 }
